4713ecfd4c690b90529fc9f187d03175c4cbd626
kokoasann <kokoasann@gmail.com>
2019-10-08 17:47:12 +0900

    enemyIA

--- kacco/Assets/Script/Enemy.cs
+++ kacco/Assets/Script/Enemy.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject Target;
+
+    NavMeshAgent nma;
     void Start()
     {
-        
+        nma = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
