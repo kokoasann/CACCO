b6476eb88b46bb0e43795529917c3c4a10b9c30e
kbc18b19 <kbc18b19@stu.kawahara.ac.jp>
2019-10-09 16:03:58 +0900

    Playerの処理更新

--- kacco/Assets/Script/Rcacco.cs
+++ kacco/Assets/Script/Rcacco.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class Rcacco : MonoBehaviour
 {
-    public float RstickX = 0.0f;
-    public float RstickY = 0.0f;
+    public float RstickX = 0.0f;        //右スティックの横の入力量。
+    public float RstickY = 0.0f;        //右スティックの縦の入力量。
 
-    public float RTrriger = 0.0f;
+    public float RTrriger = 0.0f;       //R2ボタンの入力量。
 
-    bool isTrriger = false;
+    bool isTrriger = false;             //押しているかどうか。
+
+    public Vector3 direction;           //かっこが向いている方向。
+
+    public Vector3 RayDrc;              //レイの向き。
+
+    public GameObject LCACCO;
+
+    private Transform targetTra;
+    //　ターゲットとの距離
+    private float distanceFromTargetObj;
+
+    //Lcacco LCacco;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = gameObject.transform.forward;
+        RayDrc = gameObject.transform.forward;
+        RayDrc.z = RayDrc.y;
+        RayDrc.y = 0.0f;
+        //LCACCO = GameObject.Find("LCacco");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //ターゲットとの距離。
+        distanceFromTargetObj = Vector3.Distance(transform.position, LCACCO.GetComponent<Lcacco>().transform.position);
+
         RstickX = Input.GetAxis("Horizontal2");
         RstickY = Input.GetAxis("Vertical2");
 
@@ -35,23 +54,82 @@ public class Rcacco : MonoBehaviour
 
         var rot = gameObject.transform.rotation;
         var addrot = Quaternion.identity;
+        //R1ボタンを押したときの処理。
         if (Input.GetKeyDown(KeyCode.JoystickButton5))
         {
             Debug.Log("button5");
-            addrot = Quaternion.Euler(0f, 0f, 90f); 
+            addrot = Quaternion.Euler(0f, 0f, 90f);
+            direction = gameObject.transform.forward;
+            //RayDrc = addrot * RayDrc;
+            if(RayDrc.z >= 0.5f)
+            {
+                //Debug.Log("aaaaaaaaaaaaaaaa");
+                RayDrc.x = 1.0f;
+                RayDrc.y = 0.0f;
+                RayDrc.z = 0.0f;
+            }
+            else if(RayDrc.x >= 0.5f)
+            {
+                RayDrc.z = -1.0f;
+                RayDrc.y = 0.0f;
+                RayDrc.x = 0.0f;
+            }
+            else if(RayDrc.z <= -0.5f)
+            {
+                RayDrc.x = -1.0f;
+                RayDrc.y = 0.0f;
+                RayDrc.z = 0.0f;
+            }
+            else if(RayDrc.x <= -0.5f)
+            {
+                RayDrc.z = 1.0f;
+                RayDrc.y = 0.0f;
+                RayDrc.x = 0.0f;
+            }
         }
+        //R2ボタンを押したときの処理。
         if (RTrriger <= -0.5 && isTrriger == false)
         {
             Debug.Log("button17");
             addrot = Quaternion.Euler(0f, 0f, -90f);
+            direction = gameObject.transform.forward;
+            //RayDrc = addrot * RayDrc;
             isTrriger = true;
         }
+        //R2ボタンを離したときの処理。
         else if (RTrriger >= -0.5)
         {
             isTrriger = false;
         }
+        //var lCacco = FindObjectOfType<Lcacco>();
+        RaycastHit hit = new RaycastHit();
+        if (Input.GetKey(KeyCode.JoystickButton1))
+        {
+            Debug.Log("Bボタンが押された。");
+           
+            //　Cubeのレイを飛ばしターゲットと接触しているか判定
+            Debug.Log(RayDrc);
+            if (Physics.BoxCast(this.transform.position, Vector3.one * 1.0f, RayDrc, out hit, Quaternion.identity, 1000f, LayerMask.GetMask("Target")))
+            {
+                Vector3 LDrc;
+                LDrc = LCACCO.GetComponent<Lcacco>().RayDrc;
+                Debug.Log("右から左にレイが当たった");
+                if(RayDrc == LDrc)
+                {
+                    Debug.Log("ここに敵が消滅する処理");
+                }
+            }
 
+        }
+       
+   
         gameObject.transform.position = moveSpeed;
         gameObject.transform.rotation = rot * addrot;
+    }
+    void OnDrawGizmos()
+    {
+        //　Cubeのレイを疑似的に視覚化
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + transform.forward * distanceFromTargetObj, Vector3.one);
     }
 }
