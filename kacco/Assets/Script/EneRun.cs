1487b4f620c62a10654ca0523d40a87c74f8a875
kokoasann <kokoasann@gmail.com>
2019-10-09 16:09:05 +0900

    teke

---
+++ kacco/Assets/Script/EneRun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EneRun : Enemy
+{
+    public override void TargetAction()
+    {
+        if (!isfirst)
+        {
+            Vector3 tarpo = new Vector3();
+            Vector3 tarto = new Vector3();
+
+            foreach (var ta in Target)
+            {
+                var to = ta - LR[LRnum].transform.position;
+                if (to.magnitude > tarto.magnitude)
+                {
+                    tarto = to;
+                    tarpo = LR[LRnum].transform.position;
+                }
+            }
+            nma.SetDestination(tarpo);
+            isfirst = false;
+        }
+        var tor = LR[LRnum].transform.position - transform.position;
+        if (tor.magnitude >50)
+        {
+            isLook = false;
+        }
+    }
+}
