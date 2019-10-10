ff9dc8b1bf9babc347bc30775433cee11aec1dc8
kokoasann <kokoasann@gmail.com>
2019-10-10 16:23:39 +0900

    result

--- kacco/Assets/Script/GameMaster.cs
+++ kacco/Assets/Script/GameMaster.cs
@@ -6,20 +6,44 @@ using UnityEngine.SceneManagement;
 public class GameMaster : MonoBehaviour
 {
     public GameObject EnemyFamiry = null;
-    
+    public GameObject[] LR = new GameObject[2];
+    Lcacco LC;
+    Rcacco RC;
+
+    public float time = 0.0f;
+    public float HP = 0.0f;
+    bool isResult = false;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
     
     void Start()
     {
-        
+        LC = LR[0].GetComponent<Lcacco>();
+        RC = LR[1].GetComponent<Rcacco>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(EnemyFamiry.transform.childCount == 0)
+        if (!isResult)
         {
-            SceneManager.LoadSceneAsync("Scene/ResultScene");
-            SceneManager.UnloadSceneAsync("Scene/SampleScene");
+            time += Time.deltaTime;
+            if (EnemyFamiry.transform.childCount == 0)
+            {
+                HP = LC.LHP + RC.RHP;
+                isResult = true;
+                SceneManager.LoadSceneAsync("Scene/ResultScene");
+                SceneManager.UnloadSceneAsync("Scene/SampleScene");
+            }
+            
         }
+    }
+
+    public void Deth()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ kacco/Assets/Script/Result/Result.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Result : MonoBehaviour
+{
+
+    public GameObject time;
+    public GameObject HP;
+    public GameObject score;
+    TextEditor t_time;
+    TextEditor t_HP;
+    TextEditor t_score;
+
+    GameMaster GM;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        t_time = time.GetComponent<TextEditor>();
+        t_HP = HP.GetComponent<TextEditor>();
+        t_score = score.GetComponent<TextEditor>();
+        GM = GameObject.Find("GameMaster").GetComponent<GameMaster>();
+        int m = (int)(GM.time/60f);
+        float s = GM.time % 60f;
+        t_time.text += m.ToString() + ":" + s.ToString();
+        t_HP.text += GM.HP.ToString();
+        t_score.text += (GM.HP * GM.time).ToString();
+
+        GM.Deth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            SceneManager.LoadScene("Scenes/TitleScene");
+            SceneManager.UnloadSceneAsync("Scenes/ResultScene");
+            Destroy(this);
+        }
+    }
+}
